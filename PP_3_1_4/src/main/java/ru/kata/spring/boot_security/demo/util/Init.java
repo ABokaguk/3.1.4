caa0f5c75610755fcdf2c76835d141c2a279e325
ABokaguk <118669768+ABokaguk@users.noreply.github.com>
2023-02-10 10:16:25 +0900

    Add files via upload

---
+++ PP_3_1_4/src/main/java/ru/kata/spring/boot_security/demo/util/Init.java
@@ -0,0 +1,52 @@
+package ru.kata.spring.boot_security.demo.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserService;
+
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+public class Init {
+    private final UserService userService;
+    private final RoleService roleService;
+
+    @Autowired
+    public Init(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @PostConstruct
+    private void dataBaseInit() {
+        Role roleAdmin = new Role("ADMIN");
+        Role roleUser = new Role("USER");
+        Set<Role> adminSet = new HashSet<>();
+        Set<Role> userSet = new HashSet<>();
+
+        roleService.addRole(roleAdmin);
+        roleService.addRole(roleUser);
+
+        adminSet.add(roleAdmin);
+        adminSet.add(roleUser);
+        userSet.add(roleUser);
+
+        User newUser = new User("Draco", "Malfoy", 30, "user@mail.ru", "user",
+                "user", userSet);
+        User admin = new User("Harry", "Potter", 40, "admin@mail.ru", "admin",
+                "admin", adminSet);
+
+
+        User adminGreg = new User("Dobby", "Free", 40, "test@mail.ru", "test",
+                "test", adminSet);
+
+        userService.saveUser(newUser);
+        userService.saveUser(admin);
+        userService.saveUser(adminGreg);
+    }
+}
